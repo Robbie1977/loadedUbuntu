619e431c0d01c229fd3642c251318a71fc7f8b00
Rob Court <robbie.court@gmail.com>
2018-10-02 14:32:04 +0100

    Update neo4j-flybase2neo-expression_runner.sh

--- scripts/neo4j-flybase2neo-expression_runner.sh
+++ scripts/neo4j-flybase2neo-expression_runner.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-cd /opt/VFB_neo4j/src/
+cd /opt/VFB_neo4j/
 git pull
 export PYTHONPATH=/opt/VFB_neo4j/src
 python3 -m uk.ac.ebi.vfb.neo4j.flybase2neo.expression_runner.py $PDBSERVER $PDBuser $PDBpassword /import/
