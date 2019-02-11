7be7d26cc4d6e737bd9d0f3c19e3bf62d7af0752
Rob Court <robbie.court@gmail.com>
2019-02-11 16:08:01 +0000

    updating all git repos on startup

--- scripts/startup.sh
+++ scripts/startup.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
+find / | grep '/\.git$' | rev | cut -c 6- | rev | xargs -t -n 1 git pull || :
+
 /root/anaconda3/bin/jupyter lab --ip='*' --NotebookApp.password="${JUPYPASS}" --no-browser --port 80 --allow-root
