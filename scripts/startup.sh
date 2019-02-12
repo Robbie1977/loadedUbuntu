63723166b0bb2441f8c395170190cbea6662b25e
Rob Court <robbie.court@gmail.com>
2019-02-12 12:32:24 +0000

    syntax fix

--- scripts/startup.sh
+++ scripts/startup.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-find / | grep '/\.git$' | rev | cut -c 6- | rev | xargs -t -n 1 git pull || :
+for folder in $(find / | grep '/\.git$') ; do git --git-dir=$folder pull || : ; done;
 
 /root/anaconda3/bin/jupyter lab --ip='*' --NotebookApp.password="${JUPYPASS}" --no-browser --port 80 --allow-root
