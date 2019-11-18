33d138f25f00f646222ba31ca2fb9d1a82601bcb
Rob Court <robbie.court@gmail.com>
2019-11-18 18:01:11 +0000

    removing jupiter

--- scripts/startup.sh
+++ scripts/startup.sh
@@ -2,4 +2,10 @@
 
 for folder in $(find / | grep '/\.git$') ; do git --git-dir=$folder pull || : ; done;
 
-/root/anaconda3/bin/jupyter lab --ip='*' --NotebookApp.password="${JUPYPASS}" --no-browser --port 80 --allow-root
+# /root/anaconda3/bin/jupyter lab --ip='*' --NotebookApp.password="${JUPYPASS}" --no-browser --port 80 --allow-root
+
+while true 
+do 
+  sleep 999m
+done
+  
