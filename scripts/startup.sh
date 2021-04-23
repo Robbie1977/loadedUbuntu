ec38bf629daa350f7557b77680da13447fa5a53a
Rob Court <robbie.court@gmail.com>
2021-04-23 09:25:22 +0100

    limiting to opt

--- scripts/startup.sh
+++ scripts/startup.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-for folder in $(find / | grep '/\.git$') ; do git --git-dir=$folder pull || : ; done;
+for folder in $(find /opt/ | grep '/\.git$') ; do git --git-dir=$folder pull || : ; done;
 
 # /root/anaconda3/bin/jupyter lab --ip='*' --NotebookApp.password="${JUPYPASS}" --no-browser --port 80 --allow-root
 
