b78500b4b6cae668c536edcd8938d8a2aecdc78e
Rob Court <robbie.court@gmail.com>
2024-08-08 10:19:44 +0100

    Update startup.sh

--- scripts/startup.sh
+++ scripts/startup.sh
@@ -1,19 +1,5 @@
 #!/bin/bash
 
-TIMEOUT_DURATION=30
-DELAY_DURATION=50
-
-# Function to perform git pulls with timeout and delay
-perform_git_pulls() {
-    for folder in $(find / -type d -name ".git"); do
-        # Perform the git pull with a timeout
-        timeout $TIMEOUT_DURATION git --git-dir="$folder" pull || :
-        # Wait for the specified delay duration
-        sleep $DELAY_DURATION
-    done
-}
-
-# Run the perform_git_pulls function as a subprocess
-(perform_git_pulls &)
+for folder in $(find /opt/ | grep '/\.git$') ; do echo $folder; cd $folder/../;  git pull || : ; cd ..; done;
 
 /root/anaconda3/bin/jupyter lab --ip='*' --NotebookApp.password="${JUPYPASS}" --no-browser --port 80 --allow-root
