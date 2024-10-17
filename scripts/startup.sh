8648676ea3ba01e473533d4b88b995e73c50f506
Rob Court <robbie.court@gmail.com>
2024-10-17 17:23:16 +0100

    Update startup.sh

--- scripts/startup.sh
+++ scripts/startup.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-for folder in $(find /opt/ | grep '/\.git$') ; do echo $folder; cd $folder/../;  git pull || : ; cd ..; done; 
+for folder in $(find /opt/ | grep '/\.git$') ; do echo $folder; cd $folder/../;  git pull || : ; cd ..; sleep 10m; done; 
 
 # /root/anaconda3/bin/jupyter lab --ip='*' --NotebookApp.password="${JUPYPASS}" --no-browser --port 80 --allow-root
 
